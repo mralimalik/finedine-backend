68137f7ba1c344b32f15dbfca2b55ed1484ca724
mralimalik <aliriazahmad72@gmail.com>
2024-12-16 18:04:39 +0500

    change

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-// const url = "mongodb://localhost:27017/finedinemenu";
+const url = "mongodb://localhost:27017/finedinemenu";
 // const url ="mongodb+srv://sevun:sevun@24@cluster0.qbwmf.mongodb.net/finedinemenu?retryWrites=true&w=majority&appName=Cluster0";
 // const url = "mongodb://sevun:sevun@24@cluster0-shard-00-00.qbwmf.mongodb.net:27017,cluster0-shard-00-01.qbwmf.mongodb.net:27017,cluster0-shard-00-02.qbwmf.mongodb.net:27017/finedinemenu?ssl=true&replicaSet=atlas-10h8ht-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
 
-const url = "mongodb://sevun:sevun24@cluster0-shard-00-00.qbwmf.mongodb.net:27017,cluster0-shard-00-01.qbwmf.mongodb.net:27017,cluster0-shard-00-02.qbwmf.mongodb.net:27017/?ssl=true&replicaSet=atlas-10h8ht-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+// const url = "mongodb://sevun:sevun24@cluster0-shard-00-00.qbwmf.mongodb.net:27017,cluster0-shard-00-01.qbwmf.mongodb.net:27017,cluster0-shard-00-02.qbwmf.mongodb.net:27017/?ssl=true&replicaSet=atlas-10h8ht-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDatabase = async () => {
   try {
