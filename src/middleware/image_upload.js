284b260c960362360a2039a03895c01f597ef449
mralimalik <aliriazahmad72@gmail.com>
2024-12-16 18:44:25 +0500

    auto directory create

--- src/middleware/image_upload.js
+++ src/middleware/image_upload.js
@@ -1,8 +1,36 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Check if the directory exists, and create it if it doesn't
+const ensureDirectoryExists = (directoryPath) => {
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  }
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    const tempDir = "./public/temp";
+
+    // Ensure the directory exists
+    ensureDirectoryExists(tempDir);
+
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
